613653b3f5b14032dfff782134b3e1ee55aa99d5
Zhengheng Li <zhenghenge@gmail.com>
2021-03-11 15:56:32 +0800

    device_routines

--- testing/struct-copy/inc/device_routines.hh
+++ testing/struct-copy/inc/device_routines.hh
@@ -1,5 +1,6 @@
 #include "ImageDataField.hh"
 #include "CalibDataField.hh"
 #include "ImageFeature.hh"
+#include <cuda_runtime.h>
 
-bool gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device);
+bool gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device, cudaStream_t stream = 0);
