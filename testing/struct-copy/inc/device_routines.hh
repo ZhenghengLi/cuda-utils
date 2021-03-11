db15a6c366866003e79b1882a93e516c43c9dff8
Zhengheng Li <zhenghenge@gmail.com>
2021-03-11 16:10:38 +0800

    device_routines

--- testing/struct-copy/inc/device_routines.hh
+++ testing/struct-copy/inc/device_routines.hh
@@ -3,4 +3,4 @@
 #include "ImageFeature.hh"
 #include <cuda_runtime.h>
 
-bool gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device, cudaStream_t stream = 0);
+void gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device, cudaStream_t stream = 0);
--- testing/struct-copy/src/device_routines.cu
+++ testing/struct-copy/src/device_routines.cu
@@ -1,7 +1,14 @@
 #include "device_routines.hh"
 #include <cuda_runtime.h>
 
-bool gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device, cudaStream_t stream) {
-    //
-    return true;
+__global__ void calib_kernel(ImageDataField* image_data, CalibDataField* calib_data) {
+    int m = blockIdx.x;
+    int i = threadIdx.x;
+    int j = threadIdx.y;
+    image_data->pixel_data[m][i][j] =
+        (image_data->pixel_data[m][i][j] - calib_data->pedestal[m][i][j]) / calib_data->gain[m][i][j];
+}
+
+void gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device, cudaStream_t stream) {
+    calib_kernel<<<MOD_CNT, dim3(FRAME_H, FRAME_W), 0, stream>>>(image_data_device, calib_data_device);
 }
