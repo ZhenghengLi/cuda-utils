8678d386d8609729ccc299e5f7a67fb9fb6bb074
Zhengheng Li <zhenghenge@gmail.com>
2021-03-11 20:52:32 +0800

    bug fix

--- testing/struct-copy/src/device_routines.cu
+++ testing/struct-copy/src/device_routines.cu
@@ -4,11 +4,11 @@
 __global__ void calib_kernel(ImageDataField* image_data, CalibDataField* calib_data) {
     int m = blockIdx.x;
     int i = threadIdx.x;
-    int j = threadIdx.y;
+    int j = blockIdx.y;
     image_data->pixel_data[m][i][j] =
         (image_data->pixel_data[m][i][j] - calib_data->pedestal[m][i][j]) / calib_data->gain[m][i][j];
 }
 
 void gpu_do_calib(ImageDataField* image_data_device, CalibDataField* calib_data_device, cudaStream_t stream) {
-    calib_kernel<<<MOD_CNT, dim3(FRAME_H, FRAME_W), 0, stream>>>(image_data_device, calib_data_device);
+    calib_kernel<<<dim3(MOD_CNT, FRAME_W), FRAME_H, 0, stream>>>(image_data_device, calib_data_device);
 }
