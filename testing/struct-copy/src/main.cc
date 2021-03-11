e5faec69a9511fc80e7727d863ab132d0ecdc65c
Zhengheng Li <zhenghenge@gmail.com>
2021-03-11 14:37:54 +0800

    struct-copy

--- testing/struct-copy/src/main.cc
+++ testing/struct-copy/src/main.cc
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <cstdlib>
 #include <cuda_runtime.h>
+#include <memory>
 
 #include "ImageDataField.hh"
 
 using namespace std;
+
+class CalibData {
+public:
+    CalibData() {
+        pedestal = new float[MOD_CNT][FRAME_H][FRAME_W];
+        gain = new float[MOD_CNT][FRAME_H][FRAME_W];
+    }
+    ~CalibData() {
+        delete[] pedestal;
+        delete[] gain;
+    }
+
+public:
+    float (*pedestal)[FRAME_H][FRAME_W];
+    float (*gain)[FRAME_H][FRAME_W];
+};
 
 int main(int argc, char** argv) {
 
@@ -20,11 +38,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    char pause = 0;
-
-    cout << "before cudaSetDevice" << endl;
-    cin >> pause;
-
+    // select device and print its name on success
     if (cudaSetDevice(deviceIndex) == cudaSuccess) {
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, deviceIndex);
@@ -41,16 +55,49 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    cout << "before cudaStreamCreate" << endl;
-    cin >> pause;
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // prepare calib data
+    CalibData calib;
+    for (int m = 0; m < MOD_CNT; m++) {
+        for (int i = 0; i < FRAME_H; i++) {
+            for (int j = 0; j < FRAME_W; j++) {
+                calib.pedestal[m][i][j] = 1000 + random() % 4000;
+                calib.gain[m][i][j] = 100 + random() % 100;
+            }
+        }
+    }
+
+    // prepare image data
+    ImageDataField* image_data_host = nullptr;
+    ImageDataField* image_data_host_tmp = nullptr;
+    if (cudaMallocHost(&image_data_host, sizeof(ImageDataField)) != cudaSuccess) {
+        cerr << "cudaMallocHost failed for image_data_host." << endl;
+        return 1;
+    }
+    if (cudaMallocHost(&image_data_host_tmp, sizeof(ImageDataField)) != cudaSuccess) {
+        cerr << "cudaMallocHost failed for image_data_host_tmp." << endl;
+        return 1;
+    }
+    // init
+    for (int m = 0; m < MOD_CNT; m++) {
+        for (int i = 0; i < FRAME_H; i++) {
+            for (int j = 0; j < FRAME_W; j++) {
+                image_data_host->pixel_data[m][i][j] = 6000 + random() % 9000;
+            }
+        }
+    }
+
+    ImageDataField* image_data_device = nullptr;
 
     cudaStream_t stream;
     cudaStreamCreate(&stream);
 
-    cout << "before cudaStreamDestroy" << endl;
-    cin >> pause;
-
     cudaStreamDestroy(stream);
+
+    // clean data
+    cudaFreeHost(image_data_host);
+    cudaFreeHost(image_data_host_tmp);
 
     return 0;
 }
