b067e91d14cad4692b015e32102079e2ed91fe1e
Zhengheng Li <zhenghenge@gmail.com>
2021-03-18 16:02:28 +0800

    remove the check for __CUDACC__

--- testing/calib-test/inc/ImageFeature.hh
+++ testing/calib-test/inc/ImageFeature.hh
@@ -1,16 +1,12 @@
 #ifndef __ImageFeature_H__
 #define __ImageFeature_H__
 
-#ifndef __CUDACC__
 #include <msgpack.hpp>
-#endif
 
 struct ImageFeature {
     float global_rms;
     int peak_counts;
-#ifndef __CUDACC__
     MSGPACK_DEFINE_MAP(global_rms, peak_counts);
-#endif
 };
 
 #endif
